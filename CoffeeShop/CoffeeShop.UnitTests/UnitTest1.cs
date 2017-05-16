1e77b0833b3b82d2ae89788a04a1ce4357c8aa59
CL4U\david.silva <david.silva@zuto.com>
2017-05-16 10:33:40 +0100

    Enjoy!

---
+++ CoffeeShop/CoffeeShop.UnitTests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using Com.ManchesterDigital.CoffeeShop.Interfaces;
+using Moq;
+using NUnit.Framework;
+
+namespace CoffeeShop.UnitTests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void Test1()
+        {
+            var myCoffeeShop = new Com.ManchesterDigital.CoffeeShop.CoffeeShop();
+            Assert.IsNotNull(myCoffeeShop);
+            var x = new Mock<IAskCustomer>();
+
+            var y = x.Object;
+        }
+    }
+}
